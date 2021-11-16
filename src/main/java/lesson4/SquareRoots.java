6d0e0a4f7135e8ce81dd1e2c067e03e0833628fa
Турчин Александр <aleturchin@yandex.ru>
2021-11-16 20:40:38 +0700

    add 4 Calculating the roots of a number

--- src/main/java/lesson4/SquareRoots.java
+++ src/main/java/lesson4/SquareRoots.java
@@ -6,21 +6,31 @@ public class SquareRoots {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число, корень которого нужно вычислить:");
-        long s = square(scanner.nextInt());
-        System.out.println("Корень этого числа равен " + s);
+        while (!scanner.hasNextLong()) {
+            scanner.next();
+            System.out.println("Введено не числовое значение. Введите число:");
+        }
+        long s = square(scanner.nextLong());
+        if (s <= 0) {
+            System.out.println("Число не имеет целочисленных корней");
+        }
+        else {
+            System.out.println("Корень этого числа равен " + s);
+        }
     }
 
     public static long square(long f) {
         long resalt = f;
- /*  Цикл делит входящее число на числа меньше себя, пока результат деления не будет равен делителю.
-    Например 9 разделить на 3 равно 3 -соотвествует условию.
-  */
         for (long i = 1; i <= f; i++) {
             if (resalt/i == i) {
                 resalt = resalt / i;
             }
         }
+        if (resalt == f) {
+            return 0;
+        }
+
         return resalt;
     }
-    }
+}
 
